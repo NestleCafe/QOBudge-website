76fecbb903b035853f03ca23a8240ca55673ff87
NestleCafe <watson_xian@foxmail.com>
2021-07-12 02:29:34 +0800

    deploy

---
+++ precache-manifest.1424c13fb7ac4e0d531f1b00afe52e5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec732853ecadb31ef4dd7876345b2165",
+    "url": "/QOBudge-website/QRCODE.png"
+  },
+  {
+    "revision": "3e399dd00a46d6765a6c",
+    "url": "/QOBudge-website/css/app.2527695d.css"
+  },
+  {
+    "revision": "dd3a00199e7ca4c7a0a0a4835a83f246",
+    "url": "/QOBudge-website/index.html"
+  },
+  {
+    "revision": "3e399dd00a46d6765a6c",
+    "url": "/QOBudge-website/js/app.b2628622.js"
+  },
+  {
+    "revision": "1a62ae12b25833721ed0",
+    "url": "/QOBudge-website/js/chunk-vendors.f32808de.js"
+  },
+  {
+    "revision": "3442881cb5d20d6fb183dee93eb21d56",
+    "url": "/QOBudge-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/QOBudge-website/robots.txt"
+  }
+]);
